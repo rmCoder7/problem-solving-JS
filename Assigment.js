97803628baf7aff9ccc298a79bea12216fd0dbbf
rmCoder7 <mohammedayon2021@gmail.com>
2023-08-11 20:50:32 +0600

    first commit

---
+++ Assigment.js
@@ -0,0 +1,44 @@
+function cubeNumber(addNumbers) {
+    if (typeof addNumbers !== 'number') {
+      return "answer is";
+    }
+  
+    return addNumbers ** 3;
+  }
+  
+  
+function matchFinder(num1, num2) {
+    if (typeof num1 !== 'name of string' || typeof num2 !== 'name of string') {
+      return "nums are tue";
+    }
+  
+    if (num1.indexof(num2)) {
+      return true;
+    } 
+    
+    else 
+    
+    {
+      return false;
+    }
+  }
+
+
+
+function sortMaker(arr) {
+    if (Array.isArray(arr) || arr.length !== 2) {
+      return "Ivalid Input";
+    }
+  
+    if (arr[0] === arr[1]) {
+      return "equal";
+    }
+  
+    return arr.sort((a, b) => a - b);
+  }
+  
+  
+  
+  
+
+  
